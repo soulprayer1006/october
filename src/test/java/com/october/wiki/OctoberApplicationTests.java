4ee57db8321a77697f47a8b2fb1980161022ab65
soulprayer1006 <892549892@qq.com>
2022-03-05 11:29:21 +0800

    端口测试

---
+++ src/test/java/com/october/wiki/OctoberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.october.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctoberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
