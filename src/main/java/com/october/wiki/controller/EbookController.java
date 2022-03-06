b2190b1bfef3a77247757744ec1aec7c8578aadd
soulprayer1006 <892549892@qq.com>
2022-03-06 09:33:15 +0800

    删除demo模板，手写EbookService以及 EbookController

---
+++ src/main/java/com/october/wiki/controller/EbookController.java
@@ -0,0 +1,22 @@
+package com.october.wiki.controller;
+
+import com.october.wiki.entity.Ebook;
+import com.october.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
